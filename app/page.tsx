42bad6fba1e63af7e59336bafb6afbf7dc816989
mpascualach <mpascualacheson1@gmail.com>
2024-03-30 21:55:59 +0100

    expansions from course

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 import { useChat } from "ai/react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Chat() {
-  const {
-    messages,
-    input,
-    handleInputChange,
-    handleSubmit,
-    isLoading,
-    append,
-  } = useChat();
+  const { messages, isLoading, append } = useChat();
+
+  const [imageIsLoading, setImageIsLoading] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const [audioIsLoading, setAudioIsLoading] = useState(false);
+  const [audio, setAudio] = useState<string | null>(null);
 
   const messagesContainerRef = useRef<HTMLDivElement>(null);
 
@@ -22,9 +21,72 @@ export default function Chat() {
     }
   }, [messages]);
 
+  if (imageIsLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader">
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (image) {
+    return (
+      <div className="card w-full h-screen max-w-md py-24 mx-auto stretch">
+        {/* Image and description */}
+        <img src={`data:image/jpeg;base64,${image}`} />
+        <textarea
+          className="mt-4 w-full text-white bg-black h-64"
+          value={messages[messages.length - 1].content}
+          readOnly
+        />
+        {/* Audio generator */}
+        <div className="flex flex-col justify-center mb-2 items-center">
+          {audio && (
+            <>
+              <p>Listen to the recipe: </p>
+              <audio controls src={audio} className="w-full"></audio>
+            </>
+          )}
+          {audioIsLoading && !audio && <p>Audio is being generated...</p>}
+          {!audioIsLoading && !audio && (
+            <button
+              className="bg-orange-500 p-2 text-white rounded shadow-xl"
+              onClick={async () => {
+                setAudioIsLoading(true);
+
+                const response = await fetch("/api/audio", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    message: messages[messages.length - 1].content
+                  })
+                });
+                const audioBlob = await response.blob();
+                const audioUrl = URL.createObjectURL(audioBlob);
+                setAudio(audioUrl);
+                setAudioIsLoading(false);
+              }}
+            >
+              Generate audio
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch">
-      <div className="overflow-auto h-[88%] w-full" ref={messagesContainerRef}>
+      <div
+        className="overflow-auto mb-8 w-full"
+        ref={messagesContainerRef}
+      >
         {messages.map((m) => (
           <div
             key={m.id}
@@ -46,27 +108,45 @@ export default function Chat() {
         )}
       </div>
       <div className="fixed bottom-0 w-full max-w-md">
-        <div className="flex flex-col justify-center items-center">
-          <button
-            className="bg-blue-500 p-2 text-white rounded shadow-xl"
-            disabled={isLoading}
-            onClick={() =>
-              append({ role: "user", content: "Give me a random recipe" })
-            }
-          >
-            Random Recipe
-          </button>
-          <p className="p-4">OR</p>
+        <div className="flex flex-col justify-center mb-2 items-center">
+          {messages.length == 0 && (
+            <button
+              className="bg-blue-500 p-2 text-white rounded shadow-xl"
+              disabled={isLoading}
+              onClick={() =>
+                append({
+                  role: "user",
+                  content: "Give me a random recipe",
+                })
+              }
+            >
+              Random Recipe
+            </button>
+          )}
+          {messages.length == 2 && !isLoading && (
+            <button
+              className="bg-blue-500 p-2 text-white rounded shadow-xl"
+              disabled={isLoading}
+              onClick={async () => {
+                setImageIsLoading(true);
+                const response = await fetch("api/images", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    message: messages[messages.length - 1].content,
+                  }),
+                });
+                const data = await response.json();
+                setImage(data);
+                setImageIsLoading(false);
+              }}
+            >
+              Generate image
+            </button>
+          )}
         </div>
-        <form onSubmit={handleSubmit} className="flex justify-center">
-          <input
-            className="w-[95%] p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
-            disabled={isLoading}
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
       </div>
     </div>
   );
