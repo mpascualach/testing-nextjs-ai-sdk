90d5698248662269bba3d2dd4acb0b09f3284c58
mpascualach <mpascualacheson1@gmail.com>
2024-03-30 19:03:43 +0100

    new chef update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
-import { useRef, useEffect } from 'react';
+import { useChat } from "ai/react";
+import { useEffect, useRef } from "react";
 
 export default function Chat() {
   const {
@@ -10,35 +10,34 @@ export default function Chat() {
     handleInputChange,
     handleSubmit,
     isLoading,
-    append
+    append,
   } = useChat();
 
   const messagesContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (messagesContainerRef.current) {
-      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
     }
-  }, [messages])
-
+  }, [messages]);
 
   return (
     <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch">
       <div className="overflow-auto h-[88%] w-full" ref={messagesContainerRef}>
-        { messages.map(m => (
+        {messages.map((m) => (
           <div
             key={m.id}
-            className={`whitesapce-pre-wrap ${
-              m.role === 'user'
-              ? 'bg-green-700 p-3 m-2 rounded-lg'
-              : 'bg-slate-700 p-3 m-2 rounded-lg'
+            className={`whitespace-pre-wrap ${
+              m.role === "user"
+                ? "bg-green-700 p-3 m-2 rounded-lg"
+                : "bg-slate-700 p-3 m-2 rounded-lg"
             }`}
           >
-            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.role === "user" ? "User: " : "AI: "}
             {m.content}
           </div>
-          ))
-        }
+        ))}
 
         {isLoading && (
           <div className="flex justify-end pr-4">
@@ -46,21 +45,23 @@ export default function Chat() {
           </div>
         )}
       </div>
-
       <div className="fixed bottom-0 w-full max-w-md">
         <div className="flex flex-col justify-center items-center">
           <button
-            className="bg-blue-500 p-2- text-white rounded shadow-xl"
+            className="bg-blue-500 p-2 text-white rounded shadow-xl"
             disabled={isLoading}
-            onClick={() => append({ role: "user", content: "Give me a random recipe" })}
+            onClick={() =>
+              append({ role: "user", content: "Give me a random recipe" })
+            }
           >
-            Random recipe
+            Random Recipe
           </button>
           <p className="p-4">OR</p>
         </div>
         <form onSubmit={handleSubmit} className="flex justify-center">
           <input
-            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+            className="w-[95%] p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+            disabled={isLoading}
             value={input}
             placeholder="Say something..."
             onChange={handleInputChange}
@@ -68,5 +69,5 @@ export default function Chat() {
         </form>
       </div>
     </div>
-  )
+  );
 }
